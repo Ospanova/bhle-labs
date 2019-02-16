f98c8d65ab38321cc445c112107ac2c5b04ed12f
Aida <ospanovaida08@gmail.com>
2019-02-16 14:48:12 +0600

    Bugs are fixed

--- bhle-lab-1/src/main/scala/part1/Lab2.scala
+++ bhle-lab-1/src/main/scala/part1/Lab2.scala
@@ -1,24 +1,29 @@
 package part1
 
 // Theoretical questions: why do we need abstraction
+
+//In order to hide the implementation details and showing only functionality to the user, important things to the user.
+
 // How `traits` in Scala are used?
 
-trait Animal {
-  // Is this abstract or concrete (implemented) member?
-  def name: String
 
-  // Is this abstract or concrete (implemented) member?
-  def makeSound(): String
+trait Animal {
+    // Is this abstract or concrete (implemented) member?
+    def name: String
+
+    // Is this abstract or concrete (implemented) member?
+    def makeSound(): String
 }
 
 trait Walks {
 
-  // What does this line mean?
-  this: Animal =>
+    // What does this line mean?
+    this: Animal =>
 
-  // Is this abstract or concrete (implemented) member?
-  // Why `name` parameter is available here?
-  def walk: String = s"$name is walking"
+    // Is this abstract or concrete (implemented) member?
+    // Why `name` parameter is available here?
+    //
+    def walk: String = {s"$name is walking"}
 
 }
 
@@ -26,37 +31,44 @@ trait Walks {
 // Can Dog only extend from `Walks`?
 // Try to fix Dog, so it extends proper traits
 // Implement Dog class so it passes tests
-case class Dog(???) extends Walks
+case class Dog(dog_name: String) extends Animal with Walks {
+    override def makeSound() = "Whooof"
+
+    override def name: String = dog_name
+}
 
 // Implement Cat class so it passes tests
-case class Cat(???) extends Animal with Walks
+case class Cat(cat_name: String) extends Animal with Walks {
+    override def makeSound(): String = "Miiyaaau"
+
+    override def name: String = cat_name
+}
 
 object Lab2 extends App {
 
-  // Here we will test Dog and Cat classes
 
-  val dog1 = Dog("Ceasar")
-  val dog2 = Dog("Laika")
+    val dog1 = Dog("Ceasar")
+    val dog2 = Dog("Laika")
 
-  assert(dog1.name == "Ceasar")
-  assert(dog2.name == "Laika")
+    assert(dog1.name == "Ceasar")
+    assert(dog2.name == "Laika")
 
-  assert(dog1.makeSound() == "Whooof")
-  assert(dog2.makeSound() == "Whooof")
+    assert(dog1.makeSound() == "Whooof")
+    assert(dog2.makeSound() == "Whooof")
 
-  assert(dog1.walk == "Ceasar is walking")
-  assert(dog2.walk == "Laika is walking")
+    assert(dog1.walk == "Ceasar is walking")
+    assert(dog2.walk == "Laika is walking")
 
-  val cat1 = Cat("Tosha")
-  val cat2 = Cat("Chocolate")
+    val cat1 = Cat("Tosha")
+    val cat2 = Cat("Chocolate")
 
-  assert(cat1.name == "Tosha")
-  assert(cat2.name == "Chocolate")
+    assert(cat1.name == "Tosha")
+    assert(cat2.name == "Chocolate")
 
-  assert(cat1.makeSound() == "Miiyaaau")
-  assert(cat2.makeSound() == "Miiyaaau")
+    assert(cat1.makeSound() == "Miiyaaau")
+    assert(cat2.makeSound() == "Miiyaaau")
 
-  assert(cat1.walk == "Tosha is walking")
-  assert(cat2.walk == "Chocolate is walking")
+    assert(cat1.walk == "Tosha is walking")
+    assert(cat2.walk == "Chocolate is walking")
 
 }
